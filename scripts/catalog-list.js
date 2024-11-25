47b966d1c27e2ae7d8aad6be6d4a45ef630260e6
k7q9s <vnukovmisa72@gmail.com>
2024-11-25 21:38:37 +0200

    u4.2

---
+++ scripts/catalog-list.js
@@ -0,0 +1,22 @@
+const catalog = document.querySelector('.catalog div div')
+console.log(catalog)
+
+
+
+for (let i = 0; i < 36; i++) {
+    const item = document.createElement('div')
+    item.innerHTML = `
+    <div class="col d-flex flex-column gap-1">
+        <div class="text-success"><h6>aviable</h6></div> 
+        <img src="src/150x150.png" class="img-fluid" alt="...">
+        <div class="text-warning font-weight-bold">s s s s s</div>
+        <div class="text-truncate-s">EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...</div>
+        <div>
+            <div class="text-secondary" style="text-decoration: line-through;"><h6>$499</h6></div>
+            <div><h4>$500</h4></div>
+        </div>
+    </div>
+    `
+
+    catalog.appendChild(item)
+}
